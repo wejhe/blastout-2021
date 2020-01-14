8553922fed66cdb30867668c5d8bf9e18137ef5b
Wahyu Wijiyanto <59829693+wejhe@users.noreply.github.com>
2020-01-14 17:36:14 +0700

    Update welcome.php
    Update :
    1. Landing Page UI
    2. Spirit Form

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -37,7 +37,7 @@ require_once('intern.php');
 			</button>
 			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 				<div class="navbar-nav">
-					<a class="nav-item nav-link" href="#tentang">Tentang</a>
+					<a class="nav-item nav-link" href="#sambutan">Sambutan</a>
 					<a class="nav-item nav-link" href="#keunggulan">Keunggulan</a>
 					<a class="nav-item nav-link" href="#tim">Tim</a>
 					<a class="nav-item nav-link" href="#testimoni">Testimoni</a>
@@ -54,22 +54,35 @@ require_once('intern.php');
 	<div class="card bg-dark text-white">
   		<img src="<?php echo base_url('intern/img/balairung.jpg')?>" class="card-img" alt="balairung">
   		<div class="card-img-overlay">
-			<br><br><br><br><br><br><br><br>
-			<div class="container" style="border:4px solid white; border-radius:4px;">
-				<br><br>
-				<h2 align="center" class="card-title">Balairung Klaten Association</h2>
-				<br>
-				<h5 align="center" class="card-title">Proudly Present</h5>
-				<br>
-				<h1 align="center" class="card-title"><b>BLAST OUT 2021</b></h1>
-				<br>
-				<h4 align="center" class="card-title">" Feel The Real "</h4>
-				<br><br>
+			<br><br><br><br><br><br>
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<h4><b>Balairung Klaten Association</b></h4>
+						<p>Proudly Present</p>
+						<br>
+						<h1><strong>BLAST OUT 2021</strong></h1>
+						<p align="justify">Sebuah wahana bagi kalian para pejuang PTN tahun 2021 untuk mengasah dan melatih kemampuan serta menunjukkan seberapa siap kalian untuk lolos SBMPTN tahun 2021. Dengan sistem ujian online yang telah disempurnakan, kalian akan merasakan suasana ujian layaknya saat UTBK, sehingga kalian tidak perlu lagi merasa takut untuk menghadapi UTBK yang sebenarnya. <strong>Feel The Real!</strong></p>
+						<form>
+  							<div class="form-group">
+    							<label for="semangat">Tunjukkan semangatmu di sini!</label>
+    							<input type="text" class="form-control" id="semangat" aria-describedby="spirit">
+    							<small id="spirit" class="form-text text-muted">Contoh : Bismillah 2021 Lolos UGM! - Namamu/Asal Sekolah</small>
+  							</div>
+							<button type="submit" class="btn btn-outline-light">Submit</button>
+						</form>
+					</div>
+					<div class="col">
+						<p align="right">
+							<img class="rounded" src="<?php echo base_url('intern/img/ketua.jpg')?>" width="500" height="400" alt="brand">
+						</p>
+					</div>
+				</div>
 			</div>
   		</div>
 	</div>
 
-	<div class="jumbotron" id="tentang">
+	<div class="jumbotron" id="sambutan">
 		<div class="container">
 			<br><br>
 			<div class="row">
@@ -77,11 +90,11 @@ require_once('intern.php');
 					<img class="rounded" src="<?php echo base_url('intern/img/ketua.jpg')?>" width="500" height="400" alt="brand">
 				</div>
 				<div class="col">
-					<h4><b>Balairung Klass Try Out</b></h4>
+					<h4><b>Sambutan Ketua Panitia Blast Out 2021</b></h4>
 					<br>
 					<p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed arcu eget sem accumsan consectetur et tempor nunc. Quisque fringilla elit eu nisi tempus porttitor. Etiam elit velit, vulputate et augue eu, interdum commodo elit. Vestibulum id justo lorem. Quisque id nunc nec magna iaculis facilisis ac ut felis. Ut commodo eros sit amet lorem consequat, sed fermentum orci feugiat. Nunc pellentesque metus augue, nec aliquam nunc consequat ac. Suspendisse felis turpis, maximus ut purus sed, consectetur volutpat sapien. Cras porta gravida venenatis. Duis commodo vitae ligula et rutrum. Vestibulum bibendum purus et interdum mollis. Vivamus tempus, lorem a mollis ullamcorper, felis est feugiat magna, sit amet accumsan libero purus ac arcu. Duis pharetra lorem et commodo elementum.</p>
 					<br>
-					<p align="right"><strong>Yoel Adisatya - Ketua Panitia Blast Out 2021</strong></p>
+					<p align="right"><strong>Yoel Adisatya - Teknik Geomatika 2018</strong></p>
 				</div>
  			</div>
 		</div>
